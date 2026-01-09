be1c8755970d8b74c086c94102628ec04dd8895b
sdev <stukalov.dev@proton.me>
2026-01-10 02:09:15 +0300

    fix: add theme file

--- home-modules/desktops/hyprland/waybar/default.nix
+++ home-modules/desktops/hyprland/waybar/default.nix
@@ -8,4 +8,5 @@
 
   home.file.".config/waybar/config.jsonc".source = ./config.jsonc;
   home.file.".config/waybar/style.css".source = ./style.css;
+  home.file.".config/waybar/theme.css".source = ./theme.css;
 }
