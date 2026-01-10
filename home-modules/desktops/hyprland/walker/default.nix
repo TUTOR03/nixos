ea92c9185c49249f5629a10c98e083f7b877f8d6
sdev <stukalov.dev@proton.me>
2026-01-11 02:26:18 +0300

    feature: try to style walker

--- home-modules/desktops/hyprland/walker/default.nix
+++ home-modules/desktops/hyprland/walker/default.nix
@@ -4,4 +4,7 @@
   programs.walker = {
     enable = true;
   };
+
+  home.file.".config/walker/config.toml".source = ./config.toml;
+  home.file.".config/walker/themes/gruvbox/style.css".source = ./style.css;
 }
