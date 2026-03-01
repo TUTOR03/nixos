c2da4c4463aae00fb0816ae974eb5787002c19d8
a.stukalov <a.stukalov@vk.team>
2026-03-02 02:40:55 +0300

    fix: simplify caches

--- host-modules/caches.nix
+++ host-modules/caches.nix
@@ -1,34 +1,27 @@
 { config, lib, ... }:
 
-let
-  caches = {
+{
+  options.caches = {
     hyprland = {
-      url = "https://hyprland.cachix.org";
-      key = "hyprland.cachix.org-1:a7pgxQMzvd+8n8hGRU3sKvXKJgW05YnP7G1GK98O7UQ=";
+      enable = lib.mkEnableOption "Hyprland binary cache";
     };
 
     nix-community = {
-      url = "https://nix-community.cachix.org";
-      key = "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
+      enable = lib.mkEnableOption "Nix Community binary cache";
     };
   };
 
-  # Генерация вложенных опций для каждого кэша
-  mkCacheOptions = name: {
-    enable = lib.mkEnableOption "${name} binary cache";
+  config = {
+    nix.settings = lib.mkMerge [
+      (lib.mkIf config.caches.hyprland.enable {
+        substituters = [ "https://hyprland.cachix.org" ];
+        trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxQMzvd+8n8hGRU3sKvXKJgW05YnP7G1GK98O7UQ=" ];
+      })
+
+      (lib.mkIf config.caches.nix-community.enable {
+        substituters = [ "https://nix-community.cachix.org" ];
+        trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
+      })
+    ];
   };
-
-  # Генерация конфига для каждого кэша
-  mkCacheConfig = name: data: lib.mkIf config.caches.${name}.enable {
-    nix.settings = {
-      substituters = [ data.url ];
-      trusted-public-keys = [ data.key ];
-    };
-  };
-
-in
-{
-  options.caches = lib.mapAttrs mkCacheOptions caches;
-
-  config = lib.mkMerge (lib.mapAttrsToList mkCacheConfig caches);
 }
